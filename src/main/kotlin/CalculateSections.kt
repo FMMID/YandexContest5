dbd3ecaa7ab1d3e6c0328f599f0c7c4188043ca0
Fedotov Mikhail <fedotov.mihail.m@gmail.com>
2024-03-16 19:57:09 +0300

    completed task calculated sections

---
+++ src/main/kotlin/CalculateSections.kt
@@ -0,0 +1,31 @@
+fun calculateSections(listOfNumber: List<Long>): Pair<Int, String> {
+    val listOfSections = arrayListOf<Long>()
+    var sizeOfSections = 0
+
+    var currentSectionSize = 0L
+    var currentSectionMinElement = 0L
+
+    listOfNumber.forEach { number ->
+        if (currentSectionSize == 0L) {
+            sizeOfSections++
+            currentSectionMinElement = number
+            currentSectionSize = 1
+        } else {
+            if (number > currentSectionSize && currentSectionMinElement >= currentSectionSize + 1) {
+                currentSectionMinElement = minOf(number, currentSectionMinElement)
+                currentSectionSize++
+            } else {
+                listOfSections.add(currentSectionSize)
+                sizeOfSections++
+                currentSectionMinElement = number
+                currentSectionSize = 1
+            }
+        }
+    }
+
+    if (listOfSections.size < sizeOfSections) {
+        listOfSections.add(currentSectionSize)
+    }
+
+    return Pair(sizeOfSections, listOfSections.joinToString(" "))
+}
